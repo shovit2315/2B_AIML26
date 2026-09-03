bc6998ff85c7434b1111d76182b327e946f61aef
shovit2315 <shovit9622@gmail.com>
2026-09-03 10:20:04 +0530

    Create varsum.js

---
+++ varsum.js
@@ -0,0 +1,39 @@
+
+const app =require('./module.js);
+    
+
+)
+
+
+
+
+
+
+var a= 20;
+ var b=30;
+ var c = 40 ;
+ console.log(a+b+c)
+
+var x=20;
+if(x=20){
+    console.log("x is 20");
+
+}
+else{
+    console.log("x is not 20");
+}
+
+for(var i=0; i<5;i++){
+    console.log(i);
+}
+const arr=[1,2,3,4,5];
+for(var i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    console.log(arr[2]);
+}
+
+
+
+
+
+console.log(app.xz );
