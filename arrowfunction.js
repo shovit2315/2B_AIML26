c3297cf791bb22a1ddd7227c5f68c19a7f24b91b
shovit2315 <shovit9622@gmail.com>
2026-09-03 11:28:54 +0530

    Create arrowfunction.js

---
+++ arrowfunction.js
@@ -0,0 +1,20 @@
+// normal function
+function hello()
+{
+    console.log("hello shovit sharma");
+}
+
+
+// arrow function
+hello=()=>console.log("hello shovit sharma");
+console.log(hello());
+
+
+
+function physics(marks){
+    return`${marks}`
+}
+
+
+physics=(marks)=>`${marks}`
+console.log(physics(90));
