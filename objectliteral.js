fbb125090c91615d0bdb0b9bec8566ead9394852
shovit2315 <shovit9622@gmail.com>
2026-09-08 12:10:11 +0530

    Implement object literals with variable and computed properties
    Added object literals to demonstrate variable usage and computed property names.

---
+++ objectliteral.js
@@ -0,0 +1,20 @@
+let name = "shovit";
+let rollnumber =123;
+let data =
+{
+    name,
+    rollnumber
+}
+console.log(data.name);
+console.log(data.rollnumber);
+
+
+let f = "firstname";
+let l = "lastname";
+let meta = 
+{
+    [f]:"shovit",
+    [l]:"sharma"
+}
+console.log(meta.firstname);
+console.log(meta.lastname);
